4d89de10a8b0f24387d73d919c00b23896b70aa4
blackfenzer <6431346921@student.chula.ac.th>
2025-04-29 01:32:23 +0700

    change cookie

--- backend/app/routes/auth2.py
+++ backend/app/routes/auth2.py
@@ -175,7 +175,7 @@ async def login(
         COOKIE_NAME,
         access_token,
         httponly=True,  # Make cookie HTTP-only
-        secure=True,  # Only send over HTTPS
+        secure=False,  # Only send over HTTPS
         samesite="none",  # Protect against CSRF
         max_age=ACCESS_TOKEN_EXPIRE_MINUTES * 60,  # Convert to seconds
     )
@@ -185,7 +185,7 @@ async def login(
         "csrf_token",
         csrf_token,
         httponly=False,
-        secure=True,
+        secure=False,
         samesite="none",
         max_age=ACCESS_TOKEN_EXPIRE_MINUTES * 60,
     )
