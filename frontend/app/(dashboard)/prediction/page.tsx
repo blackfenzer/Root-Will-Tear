579b7fca02ce50c6fe4b4b4ae12cbb623c80ec47
Poonyawat Suntiniwat <ninekrub139@gmail.com>
2025-05-10 20:16:11 +0700

    Update page.tsx

--- frontend/app/(dashboard)/prediction/page.tsx
+++ frontend/app/(dashboard)/prediction/page.tsx
@@ -341,6 +341,7 @@ export default function PredictionPage() {
           transition={{ duration: 0.5 }}
           whileHover={{ boxShadow: '0px 10px 15px rgba(0, 0, 0, 0.1)' }}
         >
+<<<<<<< Updated upstream
           <div className="flex items-center justify-between mt-4">
             <motion.h2
               className="text-xl font-bold"
@@ -358,6 +359,29 @@ export default function PredictionPage() {
               <ArrowRightLeft size={16} />
               {isMultipleData ? 'Input Single Data' : 'Input Multiple Data'}
             </Button>
+=======
+          <div className="flex flex-row justify-between">
+            <div className="flex justify-center mt-4">
+              <motion.h2
+                className="text-xl font-bold"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.2 }}
+              >
+                Input
+              </motion.h2>
+            </div>
+            <div className="flex justify-center mt-4">
+              {/* Mode toggle button */}
+              <Button
+                onClick={handleToggleMode}
+                className="flex items-center gap-2 bg-[#493DB1] text-[#FFFBFB] hover:bg-[#3d32a0]"
+              >
+                <ArrowRightLeft size={16} />
+                {isMultipleData ? 'Input Single Data' : 'Input Multiple Data'}
+              </Button>
+            </div>
+>>>>>>> Stashed changes
           </div>
 
           {isMultipleData ? (
